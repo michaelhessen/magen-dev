9e41405a6364529ad32eec0a94fc038bd3d1abc4
michaelhessen <michaelhessen@outlook.com>
2025-08-29 10:18:20 +0200

    magen 20250829

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'magen-cache-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './favicon.png',
+  './apple-touch-icon.png',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
